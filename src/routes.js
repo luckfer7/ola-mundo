e7e433dc566106ba4e416e2656689fa08e834d21
luckfer7 <lucasferreira059@gmail.com>
2024-06-10 12:03:51 -0300

    parte sobre mim criada

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./paginas/inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
+import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 
 
@@ -12,10 +14,20 @@ function AppRoutes() {
       {/* o menu de navegação está aaprecendo em todas as rotas de navegação. */}
 
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+        </Route>
+
+        {/* isso se chama rotas aninhadas. Junto com o outlet no paginapadrão */}
+
+        
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
+
+      <Rodape />
+      {/* importamos fora do roteador pois ele aparece em todas as paginas do figma. */}
     </BrowserRouter>
   );
 }
